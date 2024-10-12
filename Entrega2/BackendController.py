c38d9cae42285cf097907208366f5dcc8bcf74bf
ElSergioOliveros <s.oliverosb@uniandes.edu.co>
2024-10-12 14:41:46 -0500

    Persistencia

--- Entrega2/BackendController.py
+++ Entrega2/BackendController.py
@@ -1,11 +1,17 @@
 from flask import Flask, request, jsonify
 import PipelineController
 import pandas as pd
+import joblib
 import json
+import os
 
 app = Flask(__name__)
 
-pipeline = PipelineController.getTrainedPipeline()
+if os.path.exists(os.path.join(".", "Entrega2", "Model", "model.pkl")):
+    pipeline = joblib.load(os.path.join(".", "Entrega2", "Model", "model.pkl"))
+else:
+    pipeline = PipelineController.getTrainedPipeline()
+    joblib.dump(pipeline, os.path.join(".", "Entrega2", "Model", "model.pkl"))
 
 @app.route("/predict", methods=['POST'])
 def predict():
